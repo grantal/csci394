a532d3bc6872d83e8932b266938a904c87b1012f
Alex <AlexV348@gmail.com>
2018-02-03 00:38:58 +0000

    added some more tests

--- hw2.cc
+++ hw2.cc
@@ -14,10 +14,16 @@
     element after the last
 */
 const int* highest(int val, const int* start, const int* finish) {
+    // needs to be start + 1 so that it terminates when start and finish 
+    // are adjacent
     while (start + 1 < finish){
+        // if start and finish are right next to each other, then middle
+        // will be set to start because floor(1/2) = 0
         const int* middle = start + ((finish - start) / 2);    
         if (*middle > val){
             finish = middle;
+        // since we want the highest index, we will move start up even when
+        // middle is equal to val
         } else {
             start = middle;
         }
@@ -44,6 +50,39 @@ int main()
         }
 
     }
+
+    
+    // what if the whole array is the same number?
+    const int DATA_LEN_TC2 = 4;
+    const int data_tc2[DATA_LEN_TC2] = {7, 7, 7, 7};
+
+    for (auto search : {1, 7, 11 }) {
+        const auto where = highest(search, data_tc2, data_tc2 + DATA_LEN_TC2);
+        std::cout << "Index of elem: " << search << " is: ";
+        if (where == nullptr) {
+            std::cout << "not found!" << std::endl;
+        } else {
+            std::cout << (where-data_tc2) << std::endl;
+        }
+
+    }
+
+    // Negatives?
+    const int DATA_LEN_TC3 = 9;
+    const int data_tc3[DATA_LEN_TC3] = {-3, -3, -2, -2, -2, -1, 0, 1, 1};
+    // indexes:                          0,  1,  2,  3,  4,  5, 6, 7, 8
+
+    for (auto search : {-3, -2, -1, 0, 1, 2}) {
+        const auto where = highest(search, data_tc3, data_tc3 + DATA_LEN_TC3);
+        std::cout << "Index of elem: " << search << " is: ";
+        if (where == nullptr) {
+            std::cout << "not found!" << std::endl;
+        } else {
+            std::cout << (where-data_tc3) << std::endl;
+        }
+
+    }
+
     return 0;
 
 }
